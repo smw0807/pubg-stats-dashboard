687b60a766ddb5640a36ba3937a7e0c0dc90a254
smw0807 <smw0807@gmail.com>
2025-06-24 23:00:45 +0900

    프록시 설정 및 zustand 설치

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,17 @@ const nextConfig: NextConfig = {
   publicRuntimeConfig: {
     // 클라이언트에서도 사용할 환경 변수
   },
+  env: {
+    PORT: process.env.PORT || '3000',
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3000/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
