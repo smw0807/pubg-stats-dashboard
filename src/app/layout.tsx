1b1f40d3b9cc2fa8281d1d811a27c8f2c509b990
smw0807 <smw0807@gmail.com>
2025-06-30 22:11:21 +0900

    구글광고 메타태그

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
-import Script from 'next/script';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -36,12 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2744752399044248"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
+        <meta name="google-adsense-account" content="ca-pub-2744752399044248" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
